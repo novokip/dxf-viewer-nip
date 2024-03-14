182d19c6e6b811db52fc4d9d8f5c5005543981ab
novokreshchenov <igor.mail.ru>
2024-03-14 14:06:53 +0300

    fix package.json

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ * index.js
+ *
+ * Created by Igor.N on 14.03.2024 14:06
+ *
+ */
+import {DxfFetcher} from "./DxfFetcher"
+import {DxfViewer} from "./DxfViewer"
+
+export {DxfFetcher, DxfViewer}
+
+export default {DxfFetcher, DxfViewer}
